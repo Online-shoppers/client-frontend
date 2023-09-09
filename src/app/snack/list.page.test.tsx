ebadbe28e80374e12ef5ee27f0fffbf835542784
Aleksey Likhodievski <aleksey.likhodievski@gmail.com>
2023-09-09 22:43:39 +0300

    feat: added basic snack tests

---
+++ src/app/snack/list.page.test.tsx
@@ -0,0 +1,143 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { render, waitFor } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+
+import { ProductCategories } from 'app/product/enums/product-categories.enum';
+
+import store from 'store';
+
+import { getPageSnacks } from './api/get-page-snacks.api';
+import { SnackTypes } from './enums/snack-types.enum';
+import ListPage from './list.page';
+import { Snack } from './types/snack.type';
+
+const mockSnacks: Snack[] = [
+  {
+    id: 'id1',
+    created: 1000,
+    updated: 1000,
+    type: SnackTypes.NACHOS,
+    name: 'Hello',
+    description: 'Very yummy snack',
+    price: 100,
+    weight: 100,
+    rating: 0,
+    archived: false,
+    category: ProductCategories.SNACKS,
+    quantity: 1000,
+    image_url: 'https://google.com',
+    reviews_amount: 0,
+  },
+  {
+    id: 'id2',
+    created: 1000,
+    updated: 1000,
+    type: SnackTypes.NACHOS,
+    name: 'Hello',
+    description: 'Very yummy snack',
+    weight: 100,
+    price: 100,
+    rating: 0,
+    archived: false,
+    category: ProductCategories.SNACKS,
+    quantity: 1000,
+    image_url: 'https://google.com',
+    reviews_amount: 0,
+  },
+  {
+    id: 'id3',
+    created: 1000,
+    updated: 1000,
+    type: SnackTypes.NACHOS,
+    name: 'Hello',
+    description: 'Very yummy snack',
+    price: 100,
+    weight: 100,
+    rating: 0,
+    archived: false,
+    category: ProductCategories.SNACKS,
+    quantity: 1000,
+    image_url: 'https://google.com',
+    reviews_amount: 0,
+  },
+];
+
+const pageSnacksResponse = {
+  info: { total: mockSnacks.length },
+  items: mockSnacks,
+};
+
+jest.mock('./api/get-page-snacks.api', () => ({
+  getPageSnacks: jest.fn(() => Promise.resolve({ data: pageSnacksResponse })),
+}));
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => {
+    return {
+      t: (key: string) => key,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+  initReactI18next: {
+    type: '3rdParty',
+    init: jest.fn(),
+  },
+}));
+
+describe('SnackIdPage', () => {
+  let queryClient: QueryClient;
+
+  beforeEach(() => {
+    queryClient = new QueryClient();
+    window.scrollTo = jest.fn();
+  });
+
+  afterEach(() => {
+    queryClient.clear();
+  });
+
+  it('should have proper data', async () => {
+    const data = await getPageSnacks('price:asc', 1, 20);
+    expect(data).toStrictEqual({ data: pageSnacksResponse });
+  });
+
+  it('should render skeletons while loading', async () => {
+    const { findAllByTestId } = render(
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <MemoryRouter initialEntries={['/']}>
+            <Routes>
+              <Route path="/" element={<ListPage />} />
+            </Routes>
+          </MemoryRouter>
+        </Provider>
+      </QueryClientProvider>,
+    );
+
+    const skeletons = await findAllByTestId('product-card-skeleton');
+    expect(skeletons).toHaveLength(10);
+  });
+
+  it('should render page with data', async () => {
+    const { getAllByTestId } = render(
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <MemoryRouter initialEntries={['/']}>
+            <Routes>
+              <Route path="/" element={<ListPage />} />
+            </Routes>
+          </MemoryRouter>
+        </Provider>
+      </QueryClientProvider>,
+    );
+
+    await waitFor(() => {
+      const productElements = getAllByTestId('product-card');
+
+      expect(productElements).toHaveLength(mockSnacks.length);
+    });
+  });
+});
